785a54172b653abe15108d2d98f4b242ef55cf03
KakTys234 <168638151+KakTys234@users.noreply.github.com>
2024-06-16 11:12:41 +0300

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,25 +1,21 @@
-# Пункт 2
-immutable_var = 1, 3, False, 'Матрица', 'System'
-print(immutable_var)
-# Пункт 3
-# print(immutable_var[3])
-# immutable_var[3] = 'off'
-# print(immutable_var)
-# Изменить элементы кортежа нельзя, потому что кортеж не поддерживает обращение к элементам, т.е. они неизменяемые.
-# Если внутри кортежа есть элемент ввиде списка, то элементы самого списка менять можно.
-# Например:
-name = ([3, 0, 5], 5, 6, 7)
-print(name)
-#меняем
-name[0][1] = 4
-# получим
-print(name)
-# Пункт 4
-mutable_list =['A', 1, 25, 'matrix']
-print(mutable_list)
-mutable_list.append(True)
-print(mutable_list)
-mutable_list[3] = 'sport'
-print(mutable_list)
-mutable_list.extend(['boll'])
-print(mutable_list)
+# Вводим название города Комсомольск на Амуре
+my_string = input('В каком городе Вы проживаете? ')
+print('Ваш город ', my_string)
+
+# Выводим длинну символов введенного текста
+print('В названии города ', len(my_string), 'символов')
+
+# Выводим строку в верхнем регистре
+print('Ваш город ', my_string.upper())
+
+# Выводим строку в нижнем регистре
+print('Ваш город ', my_string.lower())
+
+# Изменяем строку, удалив все пробелы
+print('Ваш город ', my_string.replace(' ', ''))
+
+# Выводим первый символ строки
+print(my_string[0])
+
+# Выводим последний символ строки
+print(my_string[-1])
